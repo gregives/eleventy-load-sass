cc176b894a00ff4cff531ad1f2f0ae3e3d56438d
Greg Ives <gregives774@gmail.com>
2020-12-14 20:21:05 +0000

    Enable imports from input directory

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const sass = require("sass");
 
 module.exports = async function (content, options = {}) {
+  const dir = path.resolve(this.config.dir.input, path.dirname(this.resourcePath))
   const { css } = sass.renderSync({
     data: content,
+    includePaths: [dir],
     ...options,
   });
   return css.toString();
