d75222cf94f31c2f3380555b2a0d26b63fe4e0e0
Greg Ives <gregives774@gmail.com>
2021-02-08 23:08:43 +0000

    Default options to empty object

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const path = require("path");
 const sass = require("sass");
 
-module.exports = function (content, options) {
-  const includePaths = [
-    path.resolve(this.config.inputDir, path.dirname(this.resourcePath)),
-    path.resolve("node_modules"),
-    // Extend includePaths with options.includePaths
-    ...((options && options.includePaths) || []),
-  ];
+module.exports = function (content, _options = {}) {
+  const options = {
+    sass: {
+      includePaths: [
+        path.resolve(this.config.inputDir, path.dirname(this.resourcePath)),
+        path.resolve("node_modules"),
+      ],
+    },
+    ..._options,
+  };
+
   const { css } = sass.renderSync({
     data: content,
-    ...options,
-    includePaths,
+    ...options.sass,
   });
+
   return css.toString();
 };
