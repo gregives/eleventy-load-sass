16bde3cfc2dcaceb05f3e9b9bdf7b1553b853d63
John Hunter <johnhunter@users.noreply.github.com>
2021-07-26 10:22:42 +0100

    Merge sass option with default configuration (#2)

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const sass = require("sass");
 
 module.exports = function (content, _options = {}) {
   const options = {
+    ..._options,
     sass: {
       includePaths: [
         path.resolve(this.config.inputDir, path.dirname(this.resourcePath)),
         path.resolve("node_modules"),
       ],
+      ..._options.sass,
     },
-    ..._options,
   };
 
   const { css } = sass.renderSync({
